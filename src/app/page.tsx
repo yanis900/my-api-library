549473228c0a71712df49f1ca9269c3930aa30cd
yanis900 <yanis991@hotmail.com>
2024-12-17 22:17:05 +0000

    set up readme api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 "use client";
-import { ChevronDown, RotateCcw, Search, SquareCheckBig } from "lucide-react";
+import {
+  ChevronDown,
+  Loader,
+  RotateCcw,
+  Search,
+  SquareCheckBig,
+} from "lucide-react";
 import { useState } from "react";
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-gray-50">
       <header className="py-8 bg-white shadow-sm">
         <div className="container mx-auto px-4">
-          <h1 className="text-3xl font-bold text-center text-gray-800">
+          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-center">
             My API library 📚
           </h1>
         </div>
@@ -16,6 +22,7 @@ export default function Home() {
         <div className="max-w-2xl mx-auto space-y-6">
           <GithubUserAPI />
           <DontSwearAPI />
+          <ReadmeAPI />
         </div>
       </main>
 
@@ -52,7 +59,6 @@ const GithubUserAPI = () => {
       setUserInfo(null);
     }
   };
-
   // const fetchContributions = async () => {
   //   const query = `{
   //     user(login: "${username}") {
@@ -109,7 +115,21 @@ const GithubUserAPI = () => {
         <div className="border-gray-700 border-dashed border-2 text-center p-2 rounded-lg font-bold">
           GET
         </div>
-        <h3 className="text-xl font-medium text-gray-800">GitHub User API</h3>
+        <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+          GitHub User API
+        </h3>
+        {/* <div
+          className="border-gray-200 border-2 bg  rounded-full lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center overflow-hidden"
+          style={{
+            transform: "translateX(-5px)",
+          }}
+        >
+          <img
+            src="./icons/express.svg"
+            alt="icon5"
+            className="p-2 rounded-lg"
+          />
+        </div> */}
         <ChevronDown
           className={`text-gray-400 transform transition-transform ${
             isOpen ? "rotate-180" : ""
@@ -208,9 +228,17 @@ const DontSwearAPI = () => {
         <div className="border-gray-700 border-dashed border-2 text-center p-2 rounded-lg font-bold">
           POST
         </div>
-        <h3 className="text-xl font-medium text-gray-800">
+        <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
           Don&apos;t Swear API
         </h3>
+        {/* <div
+          className="border-gray-200 border-2 bg  rounded-full lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center overflow-hidden"
+          style={{
+            transform: "translateX(-5px)",
+          }}
+        >
+          <img src="./icons/hono.svg" alt="icon5" className="p-2 rounded-lg" />
+        </div> */}
         <ChevronDown
           className={`text-gray-400 transform transition-transform ${
             isOpen ? "rotate-180" : ""
@@ -270,6 +298,111 @@ const DontSwearAPI = () => {
         ) : (
           ""
         )}
+      </div>
+    </div>
+  );
+};
+
+const ReadmeAPI = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [file, setFile] = useState("");
+  const [newFile, setNewFile] = useState(null);
+
+  const refreshMessage = () => {
+    setFile("");
+    setNewFile(null);
+  };
+
+  const fetchMessageResponse = async () => {
+    const response = await fetch("https://readme-ai.yanait.workers.dev/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ content: file }),
+    });
+    const data = await response.json();
+    setNewFile(data);
+  };
+
+  return (
+    <div className="bg-white rounded-lg shadow-sm border border-gray-200 overflow-hidden transition-all duration-300 ease-in-out">
+      <div
+        className="p-6 cursor-pointer flex justify-between items-center"
+        onClick={() => setIsOpen(false)}
+      >
+        <div className="border-gray-700 border-dashed border-2 text-center p-2 rounded-lg font-bold">
+          POST
+        </div>
+        <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+          Readme API
+        </h3>
+        {/* <div
+          className="border-gray-200 border-2 bg  rounded-full lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center overflow-hidden"
+          style={{
+            transform: "translateX(-5px)",
+          }}
+        >
+          <img src="./icons/hono.svg" alt="icon5" className="p-2 rounded-lg" />
+        </div> */}
+        <Loader
+          className={`text-gray-400 transform transition-transform ${
+            isOpen ? "rotate-180" : ""
+          }`}
+        />
+      </div>
+      <div
+        className={`px-6 overflow-hidden transition-all duration-300 ease-in-out ${
+          isOpen ? "max-h-96 pb-6" : "max-h-0"
+        }`}
+      >
+        <p className="text-gray-600 mb-4">
+          Drag and drop or select the /src directory of your project
+        </p>
+        <input
+          value={file}
+          onChange={(e) => setFile(e.target.value)}
+          type="file"
+          className="w-full px-3 py-2 border border-gray-300 rounded-md mb-4"
+        />
+        <div className="flex space-x-2 mb-4 justify-end">
+          <button
+            onClick={fetchMessageResponse}
+            className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition-colors"
+          >
+            <SquareCheckBig />
+          </button>
+          <button
+            onClick={refreshMessage}
+            className="px-4 py-2 bg-gray-500 text-white rounded-md hover:bg-gray-600 transition-colors"
+          >
+            <RotateCcw />
+          </button>
+        </div>
+        <div
+          className={`border-2 border-dashed border-gray-300 text-gray-300 rounded-md p-4 ${
+            newFile ? "hidden" : "flex items-center justify-center"
+          }`}
+        >
+          ReadMe file will be created here.
+        </div>
+        {/* {messageResponse ? (
+          <div className="border border-gray-300 rounded-md p-4 place-content-center">
+            {messageResponse &&
+              Object.entries(messageResponse).map(([key, value]) => (
+                <h4
+                  className={`font-bold mb-2 text-center ${
+                    String(value).startsWith("error") ? "text-red-600" : ""
+                  }`}
+                  key={key}
+                >
+                  {key}: {String(value)}
+                </h4>
+              ))}
+          </div>
+        ) : (
+          ""
+        )} */}
       </div>
     </div>
   );
